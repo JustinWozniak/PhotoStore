91e1e22002d5acc7d80623827b02277042e7c244
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-02-04 00:15:42 -0800

    Added state hook to images div tp keep track if image has been hovered or not...

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,22 @@
-import React from "react"
+import React, {useState} from "react"
 
 function Image({className, img}) {
+    const [hovered, setHovered] = useState(false)
+    console.log(hovered)
     return (
-        <div className={`${className} image-container`}>
-            <img src={img.url} className="image-grid"/>
+        <div className={
+                `${className} image-container`
+            }
+            onMouseEnter={
+                () => setHovered(true)
+            }
+            onMouseLeave={
+                () => setHovered(false)
+        }>
+            <img src={
+                    img.url
+                }
+                className="image-grid"/>
         </div>
     )
 }
